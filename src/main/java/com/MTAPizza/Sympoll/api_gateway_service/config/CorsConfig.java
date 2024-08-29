4ee4676985ba0e2d9095912317ed34aa9c76d71a
Ronenii <ronen.glm@gmail.com>
2024-08-29 13:30:20 +0300

    Added origin

--- src/main/java/com/MTAPizza/Sympoll/api_gateway_service/config/CorsConfig.java
+++ src/main/java/com/MTAPizza/Sympoll/api_gateway_service/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080")  // Replace '*' with the specific origin
+                .allowedOrigins("http://localhost:8080", "http://localhost")
                 .allowCredentials(true)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
