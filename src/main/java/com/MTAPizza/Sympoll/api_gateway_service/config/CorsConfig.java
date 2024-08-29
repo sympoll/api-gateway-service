7702131129990c3d46b76ad002f351287d58e215
Ronenii <ronen.glm@gmail.com>
2024-08-29 11:49:29 +0300

    Allow all CORS origins

--- src/main/java/com/MTAPizza/Sympoll/api_gateway_service/config/CorsConfig.java
+++ src/main/java/com/MTAPizza/Sympoll/api_gateway_service/config/CorsConfig.java
@@ -10,10 +10,10 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("http://localhost:8080")  // Replace '*' with the specific origin
+                .allowCredentials(true)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
-                .allowCredentials(true)
-                .maxAge(3600);
+                .maxAge(3600); // Cache preflight response for 1 hour
     }
 }
