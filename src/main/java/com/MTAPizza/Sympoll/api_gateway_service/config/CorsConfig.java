267ecdb5005d9d66c1acd3750c937934f518acf8
Ronenii <ronen.glm@gmail.com>
2024-09-10 02:11:31 +0300

    - adjusted cors again

--- src/main/java/com/MTAPizza/Sympoll/api_gateway_service/config/CorsConfig.java
+++ src/main/java/com/MTAPizza/Sympoll/api_gateway_service/config/CorsConfig.java
@@ -13,7 +13,8 @@ public class CorsConfig implements WebMvcConfigurer {
                 .allowedOrigins("http://localhost:8080", "http://localhost", "http://sympoll.ddns.net")
                 .allowCredentials(true)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
+                .allowedHeaders("Authorization", "Content-Type", "Accept", "Origin", "X-Requested-With", "Access-Control-Request-Method", "Access-Control-Request-Headers")
+                .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials")
                 .maxAge(3600); // Cache preflight response for 1 hour
     }
 }
