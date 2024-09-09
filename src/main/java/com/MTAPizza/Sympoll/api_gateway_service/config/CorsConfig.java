363b40236043666ba2e7f52bafaaca56c72c3d71
Ronenii <ronen.glm@gmail.com>
2024-09-10 02:21:01 +0300

    - Re adjust CORS

--- src/main/java/com/MTAPizza/Sympoll/api_gateway_service/config/CorsConfig.java
+++ src/main/java/com/MTAPizza/Sympoll/api_gateway_service/config/CorsConfig.java
@@ -1,5 +1,7 @@
 package com.MTAPizza.Sympoll.api_gateway_service.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,14 +9,18 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
+    private static final Logger logger = LoggerFactory.getLogger(CorsConfig.class);
+
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+        logger.info("Configuring CORS...");
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:8080", "http://localhost", "http://sympoll.ddns.net")
                 .allowCredentials(true)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("Authorization", "Content-Type", "Accept", "Origin", "X-Requested-With", "Access-Control-Request-Method", "Access-Control-Request-Headers")
-                .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials")
+                .allowedHeaders("*")
                 .maxAge(3600); // Cache preflight response for 1 hour
+        logger.info("CORS configured with allowed origins: http://localhost:8080, http://localhost, http://sympoll.ddns.net"); // Cache preflight response for 1 hour
     }
 }
