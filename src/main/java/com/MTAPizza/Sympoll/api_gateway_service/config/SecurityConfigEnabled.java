e17979aa56c672d770340fca2aa18a8446dcc427
Ronenii <ronen.glm@gmail.com>
2024-09-10 02:49:24 +0300

    - disable csrf

--- src/main/java/com/MTAPizza/Sympoll/api_gateway_service/config/SecurityConfigEnabled.java
+++ src/main/java/com/MTAPizza/Sympoll/api_gateway_service/config/SecurityConfigEnabled.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,7 +15,9 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfigEnabled {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity.authorizeHttpRequests(authorize -> authorize
+        return httpSecurity
+        .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
                         .anyRequest().authenticated())
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .build();
